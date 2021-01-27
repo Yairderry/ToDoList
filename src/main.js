624c2d8c406dad2160e2f58f7d693280c165e20f
Yairderry <77237113+Yairderry@users.noreply.github.com>
2021-01-27 19:58:59 +0200

    display tasks in local storage if there's data
    also re-structured the former fuctions

--- src/main.js
+++ src/main.js
@@ -1,29 +1,43 @@
 "use strict";
 
+// check if there's data in the local storage
+let tasksString;
+
+if (localStorage.getItem('tasks') === null) {
+  tasksString = "[]"; 
+} else {
+  tasksString = localStorage.getItem('tasks');
+}
+
+let tasksJson = JSON.parse(tasksString);
+displayToDoList(tasksString);
+
 const addButton = document.querySelector('#add-button');
 
 addButton.addEventListener('click', addToDoContainer);
 
-function addToDoContainer(event) {
-  const toDoContainer = document.createElement('div');
+function displayToDoList(toDoList) {
+  for (let task of JSON.parse(toDoList)) {
+    createToDoContainer(task);
+  }
+}
+
+function addToDoContainer() {
   const input = document.querySelector('#text-input');
-  const viewSection = document.querySelector('#view');
+  const priority = document.getElementById('priority-selector');
 
-  toDoContainer.className = 'todo-container';
-
-  toDoContainer.append(createToDoPriority(), createToDoCreatedAt(), createToDoText(input));
-  viewSection.append(toDoContainer);
+  const task = createTaskObject(input, priority);
+  createToDoContainer(task);
   
   input.value = '';
   input.focus();
 }
 
-function createToDoPriority() {
-  const priority = document.getElementById('priority-selector');
+function createToDoPriority(priority) {
   const toDoPriority = document.createElement('div');
 
   toDoPriority.className = 'todo-priority';
-  toDoPriority.textContent = priority.value;
+  toDoPriority.textContent = priority;
   return toDoPriority;
 }
 
@@ -31,13 +45,39 @@ function createToDoText(input) {
   const toDoText = document.createElement('div');
 
   toDoText.className = 'todo-text';
-  toDoText.textContent = input.value;
+  toDoText.textContent = input;
   return toDoText;
 }
 
-function createToDoCreatedAt() {
+function createToDoCreatedAt(date) {
   const toDoCreatedAt = document.createElement('div');
 
-  toDoCreatedAt.textContent = new Date().toISOString().slice(0, 19).replace('T', ' ');
+  toDoCreatedAt.className = 'todo-created-at';
+  toDoCreatedAt.textContent = date;
   return toDoCreatedAt;
+}
+
+function createToDoContainer(task) {
+  const toDoContainer = document.createElement('div');
+  const viewSection = document.querySelector('#view-section');
+
+  toDoContainer.className = 'todo-container';
+
+  toDoContainer.append(
+    createToDoPriority(task['priority']),
+    createToDoCreatedAt(task['date']),
+    createToDoText(task['text']));
+  viewSection.appendChild(toDoContainer);
+}
+
+function createTaskObject(input, priority) {
+  const task = {
+    "text": input.value,
+    "priority": priority.value,
+    "date": new Date().toISOString().slice(0, 19).replace('T', ' ')
+  }
+  tasksJson.push(task);
+  localStorage.clear();
+  localStorage.setItem("tasks", JSON.stringify(tasksJson));
+  return task;
 }
